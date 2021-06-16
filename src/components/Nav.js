2c19f22437dfc05c168216ec716a0629bb2645f3
Fry-Fr <ryjlwdg89@gmail.com>
2021-06-16 00:48:58 -0400

    Nav component; reactstap

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>login</li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav;
