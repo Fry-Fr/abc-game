d2df06a3e00c1a99148a5823e08b2e56ba9e4ffa
Fry-Fr <ryjlwdg89@gmail.com>
2021-06-16 18:10:49 -0400

    progress

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const GameBoard = () => {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    alphabet = alphabet.split('');
+
+    const handleLetterClick = (event) => {
+        event.stopPropagation();
+        console.log("clicked", event.target)
+        let letter = event.target;
+        if (letter.className === "letter line-through"){
+            return letter.className = "letter";
+        }
+        return letter.className = "letter line-through"
+    }
     return(
-        <div>
-            GAMEBOARD
+        <div className="game-board">
+            <h1>GAMEBOARD</h1> 
+            {alphabet.map((letter,i) => {
+                return <div className="letter" key={i} onClick={handleLetterClick}>{letter.toUpperCase()}</div>
+            })}
         </div>
     )
 }
