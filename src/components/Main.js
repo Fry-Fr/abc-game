347e6f8fd42cd909bb75bae12a517939461d11d5
Fry-Fr <ryjlwdg89@gmail.com>
2022-02-26 06:35:08 -0500

    Modal Input

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
+import { Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const Main = ({setName}) => {
+const Main = ({ name, setName }) => {
+    const [modalState, setModalState] = useState(false);
+
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
     const handleNameClick = (e) => {
-        e.stopPropagation();
-        setName(prompt("What's your name"));
+        setModalState(true)
+    }
+    const handleOkClick = (e) => {
+        setModalState(false)
     }
     return (
+        <>
         <section className="section-description">
             <p>
                 On those boring highway commutes, there's no reason to feel bored. This app shall be here to assist you in playing the alphabet game. (Woo-Hoo).
@@ -15,8 +24,23 @@ const Main = ({setName}) => {
             <p>
                 ** This application is not designed to be used by the driver of a vehicle. **
             </p>
-            <button onClick={handleNameClick}>Enter name:</button>
+            <Button color='light' size='small' onClick={handleNameClick}>Enter name:</Button>
         </section>
+        { !modalState ? undefined 
+        : 
+        <Modal isOpen={modalState}>
+            <ModalHeader toggle={() => setModalState(false)}>
+                Enter name...
+            </ModalHeader>
+            <ModalBody>
+                <Input type='text' onChange={handleChange} value={name} />
+            </ModalBody>
+            <ModalFooter>
+                <Button onClick={handleOkClick}>Ok</Button>
+            </ModalFooter>
+        </Modal>
+         }
+        </>
     )
 }
 export default Main;
